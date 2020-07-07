274773c670e7d82aba74dbf29710a5902cddd553
jiyc <1048712715@qq.com>
2020-07-08 01:43:34 +0800

    drf_07

--- api/views.py
+++ api/views.py
@@ -1,13 +1,17 @@
 import re
 
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 # from rest_framework_jwt.serializers import jwt_encode_handler
 # from rest_framework_jwt.serializers import jwt_payload_handler
 
-from api.models import User
-from api.serializers import UserModelSerializer
+from api.models import User, Computer
+from api.paginations import MyPageNumberPagination
+from api.serializers import UserModelSerializer, ComputerModelSerializer
 from utils.response import APIResponse
 # from api.authentication import JWTAuthentication
 
@@ -30,3 +34,20 @@ class LoginAPIView(APIView):
         user_ser.is_valid(raise_exception=True)
 
         return APIResponse(data_message="ok", token=user_ser.token, results=UserModelSerializer(user_ser.obj).data)
+
+class ComputerListAPIView(ListAPIView):
+    queryset = Computer.objects.all()
+    serializer_class = ComputerModelSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["name", "price"]
+    ordering = ["price"]
+    pagination_class = MyPageNumberPagination
+
+class MyLimitPagination(LimitOffsetPagination):
+    def filter_queryset(self, request, queryset, view):
+        limit = request.query_params.get("limit")
+        if limit:
+            limit = int(limit)
+            return queryset[:limit]
+
+        return queryset
